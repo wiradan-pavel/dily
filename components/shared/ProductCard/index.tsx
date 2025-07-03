20c880d5ac6988a562ba217c002afcc00e151ae9
Pavel <wiradan@bk.ru>
2025-07-03 19:11:38 +0300

    alpha v.0.0.13

--- components/shared/ProductCard/index.tsx
+++ components/shared/ProductCard/index.tsx
@@ -24,55 +24,51 @@ const ProductCard = ({ item }: { item: IItem }) => {
     }
   };
 
-  if (item !== undefined) {
-    return (
-      <li className={style.item}>
-        <Link className={style.item__link} href={item.link}>
-          <CldImage
-            className={style.item__img}
-            src={item.img.url}
-            alt={item.img.alt}
-            width="214"
-            height="166"
-          />
-          <div className={style.item__info}>
-            <h6 className={style.item__title}>{item.title}</h6>
-            <p className={style.item__price}>
-              <span
-                className={`${
-                  item.discountPrice !== undefined && style.item__price__wrong
-                }`}
-              >
-                {addNumberSpaces(item.price)}₽
-              </span>
-              {item.discountPrice !== undefined && (
-                <span>{addNumberSpaces(item.discountPrice)}₽</span>
-              )}
-            </p>
-            <p className={style.item__location}>{item.location}</p>
-          </div>
-          <CldImage
-            className={style.item__seller}
-            src={item.seller.img.url}
-            alt={item.seller.img.alt}
-            title={item.seller.name}
-            width="25"
-            height="25"
-          />
-        </Link>
-        <span
-          className={`${style.item__like} ${
-            like ? style.item__like__active : ""
-          }`}
-          onClick={handleClickLike}
-        >
-          <LikeSvg />
-        </span>
-      </li>
-    );
-  } else {
-    return null;
-  }
+  return (
+    <li className={style.item}>
+      <Link className={style.item__link} href={item.link}>
+        <CldImage
+          className={style.item__img}
+          src={item.img.url}
+          alt={item.img.alt}
+          width="214"
+          height="166"
+        />
+        <div className={style.item__info}>
+          <h6 className={style.item__title}>{item.title}</h6>
+          <p className={style.item__price}>
+            <span
+              className={`${
+                item.discountPrice !== undefined && style.item__price__wrong
+              }`}
+            >
+              {addNumberSpaces(item.price)}₽
+            </span>
+            {item.discountPrice !== undefined && (
+              <span>{addNumberSpaces(item.discountPrice)}₽</span>
+            )}
+          </p>
+          <p className={style.item__location}>{item.location}</p>
+        </div>
+        <CldImage
+          className={style.item__seller}
+          src={item.seller.img.url}
+          alt={item.seller.img.alt}
+          title={item.seller.name}
+          width="25"
+          height="25"
+        />
+      </Link>
+      <span
+        className={`${style.item__like} ${
+          like ? style.item__like__active : ""
+        }`}
+        onClick={handleClickLike}
+      >
+        <LikeSvg />
+      </span>
+    </li>
+  );
 };
 
 export default ProductCard;
