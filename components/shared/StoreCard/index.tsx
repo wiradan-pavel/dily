dd9e0a22e47340eb0f5fc2370e0f734f9f589d1f
Pavel <wiradan@bk.ru>
2025-07-06 13:12:50 +0300

    alpha v.0.0.181 & store card check

--- components/shared/StoreCard/index.tsx
+++ components/shared/StoreCard/index.tsx
@@ -1,5 +1,3 @@
-"use client";
-import { useEffect, useState } from "react";
 import Link from "next/link";
 import { CldImage } from "next-cloudinary";
 import { RateStars } from "@/components/shared";
@@ -9,53 +7,43 @@ import { IItemStore } from "@/types/props";
 import style from "./style.module.scss";
 
 const StoreCard = ({ item }: { item: IItemStore }) => {
-  const [data, setData] = useState<IItemStore>();
-
-  useEffect(() => {
-    setData(item);
-  }, [item]);
-
-  if (data === undefined) {
-    return null;
-  } else {
-    return (
-      <li className={style.item}>
-        <Link
-          className={`${style.item__link} ${
-            style.item__link + "__" + data.styles.bgColor
-          }`}
-          href="#"
-        >
-          <div className={style.item__top}>
-            <CldImage
-              width={54}
-              height={54}
-              src={data.img.url}
-              alt={data.img.alt}
-            />
-            <p
-              className={`${style.item__top__name} ${
-                style.item__top__name + "__" + data.styles.textColor
-              }`}
-            >
-              {data.name}
-            </p>
-          </div>
-          <div
-            className={`${style.item__bottom} ${
-              style.item__bottom + "__" + data.styles.styleBtns
+  return (
+    <li className={style.item}>
+      <Link
+        className={`${style.item__link} ${
+          item.styles.bgColor === "orange" && style.item__link__orange
+        }`}
+        href="#"
+      >
+        <div className={style.item__top}>
+          <CldImage
+            width={54}
+            height={54}
+            src={item.img.url}
+            alt={item.img.alt}
+          />
+          <p
+            className={`${style.item__top__name} ${
+              style.item__top__name + "__" + item.styles.textColor
             }`}
           >
-            <div className={style.item__bottom__rate}>
-              {data.rate}
-              <RateStars rate={data.rate} />
-            </div>
-            <p className={style.item__bottom__location}>{data.location}</p>
+            {item.name}
+          </p>
+        </div>
+        <div
+          className={`${style.item__bottom} ${
+            style.item__bottom + "__" + item.styles.styleBtns
+          }`}
+        >
+          <div className={style.item__bottom__rate}>
+            {item.rate}
+            <RateStars rate={item.rate} />
           </div>
-        </Link>
-      </li>
-    );
-  }
+          <p className={style.item__bottom__location}>{item.location}</p>
+        </div>
+      </Link>
+    </li>
+  );
 };
 
 export default StoreCard;
